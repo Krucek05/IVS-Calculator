3e239620b8c442bbb2442b8750156c0d2bcb1698
RASTiK-beep <seryrastislav@gmail.com>
2025-03-26 16:22:41 +0100

    Finalised looks and also added some more functions to the buttons
    equls now work also you cant now write second argument after factorial  and also add another comma after coma alredy being there

--- src/calculator_GUI.py
+++ src/calculator_GUI.py
@@ -16,64 +16,92 @@ from PyQt6.QtCore import Qt
 
 class Window(QWidget):
 
-    isFloat = False
+    isFactorial = False
+    firstArgComma = False
+    secondArgComma = False
     argumentProcessed = 1
     firstArgument = ""
     secondArgument = ""
     operation = ""
-    result = ""
+    result = "123"
+
+
+    def equalsClicked(self):
+        if self.operation == "+":
+           #self.result = add(self.firstArgument, self.secondArgument)
+            print("add")
+        elif self.operation == "-":
+            #self.result = sub(self.firstArgument, self.secondArgument)
+            print("sub")
+        elif self.operation == "*":
+            #self.result = multiply(self.firstArgument, self.secondArgument)
+            print("multiply")
+        elif self.operation == "/":
+            #self.result = divide(self.firstArgument, self.secondArgument)
+            print("divide")
+        elif self.operation == "aˣ":
+            #self.result = power(self.firstArgument, self.secondArgument)
+            print("power")
+        elif self.operation == "√":
+            #self.result == n_root(self.firstArgument, self.secondArgument)
+            print("root")
+        elif self.operation == "!":
+            #self.result = factorial(self.firstArgument)
+            print("factorial")
+
 
     def numberClicked(self, number):
         if self.argumentProcessed == 1:
             self.firstArgument += number
             self.displayText.setText(self.firstArgument + self.operation)
-        else:
+        elif not self.isFactorial:
             self.secondArgument += number
             self.displayText.setText(self.firstArgument + self.operation + self.secondArgument)
+
+
     def clearClicked(self):
         if self.argumentProcessed == 2:
             if self.secondArgument == "":
                 self.argumentProcessed = 1
                 self.firstArgument = ""
                 self.operation = ""
-                self.isFloat = False
+                self.firstArgComma = False
                 self.displayText.clear()
             else:
                 self.secondArgument = ""
+                self.secondArgComma = False
                 self.displayText.setText(self.firstArgument + self.operation)
         else:
             self.firstArgument = ""
-            self.isFloat = False
+            self.firstArgComma = False
             self.displayText.clear()
+
 
     def operationClicked(self, operation):
         self.operation = operation
         self.argumentProcessed = 2
         self.displayText.setText(self.firstArgument + self.operation)
 
+
     def commaClicked(self):
-        self.isFloat = True
-        if self.argumentProcessed == 1:
+        if self.argumentProcessed == 1 and not self.firstArgComma:
+            self.firstArgComma = True
             self.firstArgument += ','
             self.displayText.setText(self.firstArgument)
-        else:
+        elif self.argumentProcessed == 2 and not self.secondArgComma:
+            self.secondArgComma = True
             self.secondArgument += ','
             self.displayText.setText(self.firstArgument + self.operation + self.secondArgument)
-
-    def factorialClicked(self):
-        if self.secondArgument == "":
-            if self.isFloat:
-                print("")
-                #self.result = factorial(float(self.firstArgument.replace(",", ".")))
-            else:
-                print("")
-                #self.result = factorial(int(self.firstArgument))
 
     def isResult(self, newOperation):
         self.firstArgument = self.result
         self.operation = newOperation
         self.secondArgument = ""
         self.result = ""
+        self.displayText.setText(self.firstArgument + self.operation)
+
+
+
 
 
     def buttonClick(self):
@@ -89,12 +117,22 @@ class Window(QWidget):
             self.commaClicked()
 
         elif buttonText == "!":
-            self.factorialClicked()
+            if not self.firstArgComma:
+                self.isFactorial = True
+                self.operation = buttonText
+                self.argumentProcessed = 2
+                self.displayText.setText(self.firstArgument + self.operation)
 
+        elif buttonText == "=":
+            self.equalsClicked()
+            self.isResult("")
 
         else:
             if self.secondArgument == "":
                 self.operationClicked(buttonText)
+            else:
+                self.equalsClicked()
+                self.isResult(buttonText)
 
 
 
@@ -107,17 +145,17 @@ class Window(QWidget):
 
         self.setWindowTitle('Calculator')
         self.setWindowIcon(QIcon('icon.png'))
-        self.setGeometry(300, 150, 290, 440)
-        self.setStyleSheet('background: #191919; font-size:25px')
-        font = QFont("Segoe UI Symbol", 16, QFont.Weight.Bold)
+        self.setGeometry(300, 150, 365, 440)
+        self.setStyleSheet('background: #191919')
+        font = QFont("Consolas", 22, QFont.Weight.Bold)
 
         simpleLayout = QVBoxLayout()
 
         self.displayText = QLineEdit()
         self.displayText.setAlignment(Qt.AlignmentFlag.AlignRight)
-        self.displayText.setFont(QFont("Arial", 20))  # Set font size
-        self.displayText.setReadOnly(True)  # Prevent user typing manually
-        self.displayText.setStyleSheet("background-color: #4c4c4c; color: white;"
+        self.displayText.setFont(QFont("Consolas", 35))
+        self.displayText.setReadOnly(True)
+        self.displayText.setStyleSheet("background-color: #191919; color: white;"
                                        "border-radius: 4px; border: none;")
         self.displayText.setFixedHeight(70)
         simpleLayout.addWidget(self.displayText)
@@ -125,31 +163,47 @@ class Window(QWidget):
         gridLayout = QGridLayout()
         gridLayout.setContentsMargins(0, 0, 0, 0)
         gridLayout.setSpacing(3)
+
         buttons = [
-            'aˣ', '√', '!', 'C',
-            '7', '8', '9', '/',
-            '4', '5', '6', '*',
-            '1', '2', '3', '-',
-            ',', '0', '=', '+'
+            '²√', 'a²', 'aˣ', 'C', '⌫',
+            'ˣ√', '7', '8', '9', '/',
+            '%', '4', '5', '6', '*',
+            '!', '1', '2', '3', '-',
+            'π', ',', '0', '=', '+',
         ]
 
         row, col = 0, 0
-        for buttonText in buttons:
-            button = QPushButton(buttonText)
+        for symbol in buttons:
+            color = "#333333"
+            color2 = "#656565"
+            if row < 1 and col > 2:
+                color = " #212f3d"
+                color2 =  "#34495e"
+            if '0' <= symbol and symbol <= '9':
+                color = "#424949"
+                color2 = "#7f8c8d"
+            if (row > 0 and col > 3) or (row == 0 and col <3) or (col < 1 and row < 4):
+                color = "#2e4053"
+                color2 = "#5d6d7e"
+
+            button = QPushButton(symbol)
             button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
             button.setFont(font)
-            # noinspection PyUnresolvedReferences
-            button.clicked.connect(self.buttonClick)
+            button.clicked.connect(self.buttonClick)  # Connect to function
             button.setStyleSheet("QPushButton {"
-                                 "background-color: #333333; color: white;"
+                                 "background-color: %s; color: white;"
                                  "border-radius: 4px; border:none;}"
                                  "QPushButton:hover {"
-                                 "background-color: #656565; color: white;}")
-            gridLayout.addWidget(button, row, col)
+                                 "background-color: %s; color: white;}" %(color, color2))
+
+            gridLayout.addWidget(button, row, col)  # Add button to the layout
             col += 1
-            if col > 3:
-                col = 0
+            if col > 4:
                 row += 1
+                col = 0
+
+
+
 
         simpleLayout.addLayout(gridLayout)
         self.setLayout(simpleLayout)
