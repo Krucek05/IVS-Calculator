0e85e9779e2ee0ce8110a72dbeb5f237a1e57923
Filip Figúr <xfigurf00@vutbr.fit.cz>
2025-03-27 15:00:54 +0100

    added math library v0.1

---
+++ src/math_library.py
@@ -0,0 +1,42 @@
+############################################################################
+# @file math_library.py
+# @brief Calculator math library for IVS 2025
+# @date 27.3.2022
+# @author: Filip Figúr <xfigurf00>
+#
+# Implemenation of tests for calculator library
+############################################################################
+
+import math
+
+def add(a,b):
+    return (a+b)
+
+def sub(a,b):
+    return (a-b)
+
+def multiply(a,b):
+    return(a*b)
+
+def divide(a,b):
+    return(a/b)
+
+def power(a,b):
+    return (a^b)
+
+def n_root(a,b):
+    return(a^(1/b))
+
+def factorial(a):
+    return(a*a) #TODO
+
+def modulo(a,b):
+    return (a%b)  
+
+
+
+
+
+
+    
+ 
