596ba01fa3bef4752a8dc9aa89811d01ab778d2e
Filip Figúr <xfigurf00@vutbr.fit.cz>
2025-04-04 23:03:13 +0200

    Added factorial and modulo

--- src/math_library.py
+++ src/math_library.py
@@ -8,6 +8,7 @@
 ############################################################################
 
 import math
+from decimal import Decimal
 """
 @brief ADD function
 """
@@ -27,36 +28,53 @@ def power(a,b):
     return (a**b)
 
 def n_root(a,b):
+    if ((isinstance(a,float)) or (isinstance(b,float))):
+        raise ValueError("Floats are not allowed\n")
+    if ((a < 0) and (b % 2 == 0)):#n_root(-8,2)
+        raise ValueError("Cannot compute\n")
+    if (((a != 0) and (b == 0)) or ((a == 0) and (b == 0))): #n_root(2,0) or #n_root(0,0)
+        raise ValueError("Undefined")
+    
+    
     return(a**(1/b))
+
 """
 Limit for factorial should be enough
 """
 def factorial(a):
-    if isinstance(a,float):
+    if (isinstance(a,float)):
         raise ValueError("Floats are not allowed")
-    if a < 0:
+    if (a < 0):
         raise ValueError("Factorial expects natural number or zero\n")
-    if a > 20:
+    if (a > 20):
         raise ValueError("Overflow\n")
-    if a == 0 or a == 1:
+    if ((a == 0) or (a == 1)):
         return 1
     result = 1
     counter = 1
-    while (counter <= (a)):
+    while (counter <= a):
         result *= counter
         counter += 1
-    return(result) 
+    return(result)
+
 """
 Formula for modulo: Dividend mod Divisor = Remainder
 Proof:  Quotient * Divisor + Remainder = Dividend
 """
 def modulo(a,b):
-    return (a%b)  
-
-
-
-
-
-
-    
- 
+    if (b == 0):
+        raise ValueError("Division by zero\n")
+    if ((isinstance(a,float)) or (isinstance(b,float))):
+        a = Decimal(str(a))
+        b = Decimal(str(b))
+        result = a % b
+        # result should be positive if b is positive
+        if ((result < 0) and (b > 0)):
+            result += b
+        # result should be positive if b is negative
+        elif ((result > 0) and (b < 0)):
+            result -= b
+        return (result) 
+    else:
+        result = a % b
+        return (result)
