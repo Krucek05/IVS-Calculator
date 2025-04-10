9b9d2c80b12be27e2d5bc4d1a59909cf13c86107
Filip Figúr <xfigurf00@vutbr.fit.cz>
2025-04-10 21:09:23 +0200

    Added n_root, library almost complete

--- src/math_library.py
+++ src/math_library.py
@@ -28,15 +28,24 @@ def power(a,b):
     return (a**b)
 
 def n_root(a,b):
-    if ((isinstance(a,float)) or (isinstance(b,float))):
-        raise ValueError("Floats are not allowed\n")
+    #if ((isinstance(a,float)) or (isinstance(b,float))):
+    #    raise ValueError("Floats are not allowed\n")
     if ((a < 0) and (b % 2 == 0)):#n_root(-8,2)
         raise ValueError("Cannot compute\n")
-    if (((a != 0) and (b == 0)) or ((a == 0) and (b == 0))): #n_root(2,0) or #n_root(0,0)
-        raise ValueError("Undefined")
+    if ((a < 0) and (b < 0)):
+        raise ValueError("Cannot compute\n")
+    if (((a != 0) and (b == 0)) or ((a == 0) and (b == 0)) or ((a == 0) and (b < 0))): #n_root(2,0) or #n_root(0,0)
+        raise ValueError("Undefined\n")
+    if ((a == 0) and (b == 0)):
+        raise ValueError("Undefined\n")
     
-    
-    return(a**(1/b))
+    if a < 0:
+        root = -((-a) ** (1 / b))
+    else:
+        root = a ** (1 / b)
+    if math.isclose(root, round(root), abs_tol=1e-9):
+        return round(root)
+    return root
 
 """
 Limit for factorial should be enough
@@ -71,7 +80,7 @@ def modulo(a,b):
         # result should be positive if b is positive
         if ((result < 0) and (b > 0)):
             result += b
-        # result should be positive if b is negative
+        # result should be negative if b is negative
         elif ((result > 0) and (b < 0)):
             result -= b
         return (result) 
