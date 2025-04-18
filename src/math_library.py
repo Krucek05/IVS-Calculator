0e65a303cd99242f2db4c9eec8823a496dd5a029
Filip Figúr <xfigurf00@vutbr.fit.cz>
2025-04-18 12:37:53 +0200

    Comments added

--- src/math_library.py
+++ src/math_library.py
@@ -1,7 +1,7 @@
 ############################################################################
 # @file math_library.py
 # @brief Calculator math library for IVS 2025
-# @date 27.3.2022
+# @date 27.3.2025
 # @author: Filip Figúr <xfigurf00>
 #
 # Implemenation of tests for calculator library
@@ -12,50 +12,80 @@ from decimal import Decimal
 """
 @brief ADD function
 """
+# @brief Adds two numbers
+# @param a First operand
+# @param b Second operand
+# @return The result of a+b (Sum)
 def add(a,b):
     return (a+b)
 
+
+# @brief Substraction of two numbers
+# @param a First operand (minuend)
+# @param b Second operand (subtrahend)
+# @return The result of a-b (difference)
 def sub(a,b):
     return (a-b)
 
+# @brief Multiplication of two numbers
+# @param a First operand (Multiplicand)
+# @param b Second operand (Multiplier)
+# @return The result of a*b (Product)
 def multiply(a,b):
     return(a*b)
 
+# @brief Division of two numbers
+# @param a First operand (Dividend)
+# @param b Second operand (Divisor)
+# @return The result of a/b (Quotient)
 def divide(a,b):
     return(a/b)
 
+# @brief Raised the number (a) to the power of an exponent (b)
+# @param a Base number
+# @param b Exponent
+# @return The result of a raised to the power of b (a^b)
 def power(a,b):
     return (a**b)
 
+# @brief Calculates the b-th root of number a (a and b are real numbers)
+# @param a Number under root (radicand)
+# @param b Degree of the root (Non-zero integer)
+# @return The real b-th root of a 
+# @throws ValueError if the operation is undefined or would require complex numbers
 def n_root(a,b):
-    #if ((isinstance(a,float)) or (isinstance(b,float))):
-    #    raise ValueError("Floats are not allowed\n")
-    if ((a < 0) and (b % 2 == 0)):#n_root(-8,2)
+    # CASE Not allowed - complex numbers needed
+    if ((a < 0) and (b % 2 == 0)): #n_root(-8,2)
         raise ValueError("Cannot compute\n")
-    if ((a < 0) and (b < 0)):
+    if ((a < 0) and (b < 0)): #n_root(-8,-2)
         raise ValueError("Cannot compute\n")
-    if (((a != 0) and (b == 0)) or ((a == 0) and (b == 0)) or ((a == 0) and (b < 0))): #n_root(2,0) or #n_root(0,0)
+    # CASE Undefined - mostly a^(1/0)
+    if ((a != 0) and (b == 0)): #n_root(2,0)
         raise ValueError("Undefined\n")
-    if ((a == 0) and (b == 0)):
+    if ((a == 0) and (b == 0)): #n_root(0,0)
         raise ValueError("Undefined\n")
-    
-    if a < 0:
+    if ((a == 0) and (b < 0)): #n_root(0,-1)
+        raise ValueError("Undefined\n")
+    # Valid Input - Compute real root
+    if a < 0: # n_root(-27, 3) -> -(-(-27)^(1/3)) = -3 
         root = -((-a) ** (1 / b))
     else:
         root = a ** (1 / b)
+    # Round when result is very close to an integer
     if math.isclose(root, round(root), abs_tol=1e-9):
         return round(root)
     return root
 
-"""
-Limit for factorial should be enough
-"""
+# @brief Calculates factorial of positive integer a
+# @param a Positive integer and (a < 20)
+# @return Factorial of number a
+# @throws ValueError if the input is float, negative or greater than 20
 def factorial(a):
-    if (isinstance(a,float)):
+    if (isinstance(a,float)): # Basic factorial function is defined only for positive integers
         raise ValueError("Floats are not allowed")
-    if (a < 0):
-        raise ValueError("Factorial expects natural number or zero\n")
-    if (a > 20):
+    if (a < 0): 
+        raise ValueError("Factorial expects positive integer or zero\n")
+    if (a > 20): # 20! = 2_432_902_008_176_640_000, it should be enough
         raise ValueError("Overflow\n")
     if ((a == 0) or (a == 1)):
         return 1
@@ -66,10 +96,13 @@ def factorial(a):
         counter += 1
     return(result)
 
-"""
-Formula for modulo: Dividend mod Divisor = Remainder
-Proof:  Quotient * Divisor + Remainder = Dividend
-"""
+# @brief Calculates modulo of a/b
+#        In Python, result of modulo (%) has same sign as the divisor
+#        Formula for modulo: Dividend mod Divisor = Remainder
+#        Proof:  Quotient * Divisor + Remainder = Dividend
+# @param a First operand (Dividend)
+# @param b Second operand (Divisor)
+# @return The result of a%b (Remainder)
 def modulo(a,b):
     if (b == 0):
         raise ValueError("Division by zero\n")
@@ -77,10 +110,10 @@ def modulo(a,b):
         a = Decimal(str(a))
         b = Decimal(str(b))
         result = a % b
-        # result should be positive if b is positive
+        # Result should be positive if b is positive
         if ((result < 0) and (b > 0)):
             result += b
-        # result should be negative if b is negative
+        # Result should be negative if b is negative
         elif ((result > 0) and (b < 0)):
             result -= b
         return (result) 
